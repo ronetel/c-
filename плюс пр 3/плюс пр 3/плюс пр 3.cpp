7812763ed7a0d906a6eccf75fc0882aacda74cb6
ronetel <144216054+ronetel@users.noreply.github.com>
2024-02-21 14:34:03 +0300

    практическая номер 3

---
+++ плюс пр 3/плюс пр 3/плюс пр 3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+const int ROWS = 3;
+const int COLS = 5;
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    double arr[ROWS][COLS];
+
+    for (int i = 0; i < ROWS; ++i) {
+        std::cout << "Введите " << COLS << " элементов для " << i + 1 << "-й строки:" << std::endl;
+        for (int j = 0; j < COLS; ++j) {
+            std::cin >> arr[i][j];
+        }
+    }
+
+    for (int i = 0; i < ROWS; ++i) {
+        double sum = 0;
+        for (int j = 0; j < COLS; ++j) {
+            sum += arr[i][j];
+        }
+        double avg = sum / COLS;
+        std::cout << "Среднее арифметическое для " << i + 1 << "-й строки: " << avg << std::endl;
+    }
+    return 0;
+}
